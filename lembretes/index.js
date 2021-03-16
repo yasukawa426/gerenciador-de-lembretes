8972ef5dd76f1445e2b533ee549be78d780d6d25
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2021-03-15 21:51:49 -0300

    Criação inicial do projeto gerenciador de lembretes. Criado microsserviços lembretes e observaçoes, atualmente sem função.

---
+++ lembretes/index.js
@@ -0,0 +1,14 @@
+//
+const express = require('express');
+const app = express();
+
+//metodo get em /lembretes
+app.get('/lembretes', (req, res) =>{
+
+});
+
+app.put('/lembretes',(req, res) =>{
+
+});
+
+app.listen(4000, () => console.log("Lembretes:4000."));
