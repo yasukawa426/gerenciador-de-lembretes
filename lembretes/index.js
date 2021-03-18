dd7d65b0e0c96bc3f8641999bca8cc7561c6136a
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2021-03-18 20:35:11 -0300

    Requisições de get e put adicionados em lembretes/index.js.

--- lembretes/index.js
+++ lembretes/index.js
@@ -1,14 +1,22 @@
 //
-const express = require('express');
+const express = require('express');//import
 const app = express();
+app.use(express.json());//faz parse do json
+const lembretes = {};
+let contador = 0;
 
 //metodo get em /lembretes
 app.get('/lembretes', (req, res) =>{
-
+    res.send(lembretes);
 });
 
 app.put('/lembretes',(req, res) =>{
-
+    contador++;
+    const {texto} = req.body;//ja ta vindo parseado por causa da linha app.use(express.json())
+    lembretes[contador]= {
+        contador, texto
+    }
+    res.status(201).send(lembretes[contador]);
 });
 
 app.listen(4000, () => console.log("Lembretes:4000."));
