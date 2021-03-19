58d98f0b7f0a8b0909eed141dfb6643c95fc6891
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2021-03-18 21:50:00 -0300

    Barramentop de eventoscriado.

---
+++ barramento-de-eventos/index.js
@@ -0,0 +1,20 @@
+//imports
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+app.use(express.json());
+
+app.post('/eventos', (req,res) => {
+    const evento = req.body;
+    //envia o evento para lembretes
+    axios.post('http://localhost:4000/eventos', evento);
+    //envia o evento para observacoes
+    axios.post('http://localhost:5000/eventos', evento);
+    res.status(200).send({msg:"ok"});
+      
+});
+
+app.listen(10000, () => {
+    console.log('Barramento de eventos. Porta 10000');
+});
--- lembretes/index.js
+++ lembretes/index.js
@@ -1,5 +1,6 @@
 //
 const express = require('express');//import
+const axios = require('axios');
 const app = express();
 app.use(express.json());//faz parse do json
 const lembretes = {};
@@ -10,13 +11,26 @@ app.get('/lembretes', (req, res) =>{
     res.send(lembretes);
 });
 
-app.put('/lembretes',(req, res) =>{
+app.put('/lembretes', async(req, res) =>{
     contador++;
     const {texto} = req.body;//ja ta vindo parseado por causa da linha app.use(express.json())
     lembretes[contador]= {
         contador, texto
     }
+    await axios.post("http://localhost:10000/eventos",  {
+        tipo: "Lembrete criado",
+        dados: {
+            contador,
+            texto,
+        },
+    })
     res.status(201).send(lembretes[contador]);
 });
+
+app.post('/eventos', (req, res) => {
+    console.log(req.body);
+    res.status(200).send({msg: "ok"});
+});
+
 
 app.listen(4000, () => console.log("Lembretes:4000."));
